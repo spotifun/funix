8e3b63665a9d2e023be6923bcf3fc44ccf49b3a9
sage <laymonage@gmail.com>
2020-12-11 23:00:10 +0700

    project: Initial Heroku production configuration

--- lib/funix_web/endpoint.ex
+++ lib/funix_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule FunixWeb.Endpoint do
   ]
 
   socket "/socket", FunixWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
