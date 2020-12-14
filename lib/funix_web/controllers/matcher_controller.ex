c77174bfebb72eba26f09e70d34a9ce3579f6db3
Muhamad Achir Suci Ramadhan <muhamadachir99@gmail.com>
2020-12-15 02:15:42 +0700

    Add matcher destroy timer, set to 2 minutes, and fix private functions

--- lib/funix_web/controllers/matcher_controller.ex
+++ lib/funix_web/controllers/matcher_controller.ex
@@ -1,24 +1,49 @@
+defmodule FunixWeb.MatcherDestroyer do
+    use GenServer
+
+    def start_link(_name \\ nil) do
+        GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
+    end
+
+    def init([destroy_function]) do
+        Process.send_after(self(), :terminate_self, 2 * 60 * 1000) # 2 minutes
+        {:ok, [destroy_function]}
+    end
+
+    def handle_info(:terminate_self, [destroy_function]) do
+        destroy_function.()
+        {:stop, :normal, []}
+    end
+end
+
 defmodule FunixWeb.MatcherController do
     use FunixWeb, :controller
     alias Funix.{Repo, Matcher}
+    alias FunixWeb.MatcherDestroyer
 
-    def get_unique_random_id(random_number, matching_objects) when matching_objects == nil do
+    defp get_unique_random_id(random_number, matching_objects) when matching_objects == nil do
         random_number
     end
 
-    def get_unique_random_id(random_number, _) do
+    defp get_unique_random_id(_random_number, _matching_objects) do
         new_random_number = Enum.random(100_000..999_999)
         get_unique_random_id(new_random_number, Repo.get_by(Matcher, matching_id: new_random_number))
     end
 
-    def generate(conn, %{"user_id" => user_id, "search_matching_id" => search_matching_id}) do
+    def generate(conn, %{"user_id" => user_id}) do
         random_number = Enum.random(100_000..999_999)
         matching_id = get_unique_random_id(random_number, Repo.get_by(Matcher, matching_id: random_number))
         
         {status, changeset} = Matcher.changeset(%Matcher{}, %{user_id: user_id, matching_id: matching_id}) |> Repo.insert()
-
+        
         case status do
-            :ok -> json(conn, %{user_id: user_id, matching_id: matching_id, success: status})
+            :ok ->
+                destroy_function = fn () ->
+                    Repo.delete(changeset, [stale_error_field: :stale_error])
+                end
+
+                {:ok, _pid} = GenServer.start_link(MatcherDestroyer, [destroy_function])
+                json(conn, %{user_id: user_id, matching_id: matching_id, success: status})
             :error ->
                 errors = Matcher.translate_error(changeset.errors)
                 json(conn, %{user_id: user_id, matching_id: matching_id, status: status, errors: errors})
